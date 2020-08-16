e779c7e2f9a84ece9dfad621d7929ea1c2a12c69
Dhanjeet <11dhanjeet@gmail.com>
2020-08-16 17:19:52 +0530

    coomit for all

--- src/test/java/com/qa/TestPages/LoginpageTest.java
+++ src/test/java/com/qa/TestPages/LoginpageTest.java
@@ -1,5 +1,44 @@
 package com.qa.TestPages;
 
-public class LoginpageTest {
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-}
+import com.qa.Pages.HomePage;
+import com.qa.Pages.LoginPage;
+import com.qa.TestBase.TestBase;
+
+public class LoginpageTest extends TestBase{
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	@BeforeMethod
+		public void setup() {
+		Initialization();	
+		}
+	
+	@Test
+	public void titleCheck() {
+		String actual=loginPage.getTitle();
+		String expected="home page";
+		Assert.assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void getLoggedIn() {
+		String userId=prop.getProperty("userId");
+		String password=prop.getProperty("password");
+		loginPage.getLoggedin(userId, password);
+		//Assert.assertEquals(actual, expected, message);
+		
+	}
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+	}
+
+
---
+++ src/test/java/com/qa/TestPages/LoginpageTestTest.java
@@ -0,0 +1,16 @@
+package com.qa.TestPages;
+
+import org.testng.annotations.Test;
+
+public class LoginpageTestTest {
+
+  @Test
+  public void getLoggedInTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void titleCheckTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
